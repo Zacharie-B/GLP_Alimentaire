941e68085688dbac0ff4c8a8e9c3184a0304ebef
HP <HP@DESKTOP-7VPH474.home>
2020-02-08 17:54:19 +0100

    Test for display ecosystem characteristics

---
+++ GLPalimentaire/src/data/primaryConsumerdata/Buffalo.java
@@ -0,0 +1,37 @@
+package data.primaryConsumerdata;
+
+import java.util.ArrayList;
+
+import data.PrimaryConsumer;
+
+public class Buffalo extends PrimaryConsumer{
+
+	public Buffalo(int hp, ArrayList<String> eatenBy, ArrayList<String> diet, int organicMass, boolean isAlive, int populationDensity, int numberBirths, int timeBreeding, int calories, int timeExcreta, boolean sickness) {
+		super();
+		setHp(hp);
+		setEatenBy(eatenBy);
+		setDiet(diet);
+		setOrganicMass(organicMass);
+		setIsAlive(isAlive);
+		setPopulationDensity(populationDensity);
+		setNumberBirths(numberBirths);
+		setTimeBreeding(timeBreeding);
+		setCalories(calories);
+		setTimeExcreta(timeExcreta);
+		setSickness(sickness);
+		initializeEatenBy();
+		initializeDiet();
+	}
+	@Override
+	public void initializeEatenBy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void initializeDiet() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ GLPalimentaire/src/data/producersdata/TreeLeaves.java
@@ -0,0 +1,30 @@
+package data.producersdata;
+
+import java.util.ArrayList;
+
+import data.Producer;
+
+public class TreeLeaves extends Producer{
+	
+	public TreeLeaves(boolean isalive, int populationdensity, ArrayList<String> eatenBy, int numberbirths, int timebreeding,
+			int hp, int organicmass, int organicprocessingspeed, int ratemineral) {
+		super();
+		setIsAlive(isalive);
+		setPopulationDensity(populationdensity);
+		setEatenBy(eatenBy);
+		setNumberBirths(numberbirths);
+		setTimeBreeding(timebreeding);
+		setHP(hp);
+		setOrganicMass(organicmass);
+		setOrganicprocessingspeed(organicprocessingspeed);
+		setRateMineral(ratemineral);
+		initializeEatenBy();
+	}
+
+	@Override
+	public void initializeEatenBy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

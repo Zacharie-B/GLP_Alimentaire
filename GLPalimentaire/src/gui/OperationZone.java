2cbe6731ed036e198c6be0a913ded95633a2ed16
Thibaut <Thibaut@sm1-458-07>
2020-02-27 15:24:36 +0100

    GUI change 2

--- GLPalimentaire/src/gui/OperationZone.java
+++ GLPalimentaire/src/gui/OperationZone.java
@@ -5,6 +5,7 @@ import java.awt.Dimension;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.Border;
 
@@ -14,6 +15,10 @@ public class OperationZone extends JPanel{
 
 	private static final Dimension IDEAL_MAIN_DIMENSION = new Dimension(800, 400);
 	
+	//debut:test
+	private JLabel labelBoutton = new JLabel("Boutton");
+	//fin:test
+	
 	private JButton leave = new JButton("Leave");
 	private JButton info = new JButton("Information");
 	private JButton disaster = new JButton("Disaster");
@@ -21,9 +26,10 @@ public class OperationZone extends JPanel{
 	private Border lineborder = BorderFactory.createLineBorder(Color.black, 1);
 	
 	public OperationZone(){
-		add(leave);
-		add(info);
-		add(disaster);
+		//add(leave);
+		//add(info);
+		//add(disaster);
+		add(labelBoutton);
 		setBorder(lineborder);
 	}
 }

9526325ca21d0084eb733122ecb7d9e04b78a6a6
e-tbalet <e-tbalet@sm1-458-07>
2020-03-05 17:17:03 +0100

    GUI

---
+++ GLPalimentaire/src/gui/MainWindowGUI.java
@@ -0,0 +1,98 @@
+package gui;
+
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class MainWindowGUI extends JFrame{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private Dimension dimensionFenetre = new Dimension(600,200);
+	
+	private JLabel maintitre = new JLabel("Simulation Ecosystème");
+	
+	private JButton launchedDefault = new JButton("Simulation Rapide");
+	private JButton launchedCustom = new JButton("Simulation Personnalisée");
+	private JButton leave = new JButton("Quitter");
+	
+	private JPanel panel = new JPanel(new GridBagLayout());
+	
+	public MainWindowGUI(String title){
+		launchedDefault.addActionListener(new LaunchedDefault());
+		
+		launchedCustom.addActionListener(new LaunchedCustom());
+		
+		leave.addActionListener(new Leave());
+		
+		GridBagConstraints c1 = new GridBagConstraints();
+		
+		c1.gridx = 0;
+		c1.gridy = 0;
+		panel.add(maintitre, c1);
+		
+		c1.gridx = 0;
+		c1.gridy = 1;
+		panel.add(launchedDefault, c1);
+		
+		c1.gridx = 0;
+		c1.gridy = 2;
+		panel.add(launchedCustom, c1);
+		
+		c1.gridx = 0;
+		c1.gridy = 3;
+		panel.add(leave, c1);
+		
+		add(panel);
+		
+		setTitle(title);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		pack();
+		setSize(dimensionFenetre);
+		setVisible(true);
+		setResizable(false);
+	}
+	
+	public static void main(String[] args) {
+		new MainWindowGUI("Ecosystem Simulation");
+	}
+
+	public class LaunchedDefault implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			new MainGUI();
+			setVisible(false);	
+			dispose();
+		}
+		
+	}
+	
+	public class LaunchedCustom implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			
+		}
+		
+	}
+	
+	public class Leave implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			dispose();
+		}
+	
+	}
+}
